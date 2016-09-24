b47e9407ab168b3e68dbe1015702ba0b7bfeb5d8
黄书乐 <492179450@qq.com>
2016-09-24 18:26:20 +0800

    优化页面，更新职业颜色

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@
         team: false,
         box: false
       },
+      bool: false,
       infoBox: []
     },
     computed: {
@@ -22,14 +23,32 @@
       },
       treasure: function (arr) {
         if (arr.point) {
-          return arr.ren + ' ' + arr.point + '分 ' + arr.name
+          return ' ' + arr.point + '分 '
         } else {
-          return arr.name + ' 被分解'
+          return ' 分解'
         }
       },
       showInfo: function (index) {
         vm.infoBox = vm.table[index]
         vm.status.box = true
+      },
+      proColor: function (pro) {
+        return eng[chinese.indexOf(pro)]
+      },
+      nameColor: function (name) {
+
+        var pro
+
+        for (var i = 0, dio = _gays.length; i < dio; i++) {
+          if (_gays[i].nick === name) {
+            pro = _gays[i].pro
+          }
+        }
+
+        return eng[chinese.indexOf(pro)]
+      },
+      keySort: function (key, bool, type) {
+
       },
       init: function () {
         var arr = [], table = {}, nick, name, pro
@@ -44,7 +63,16 @@
 
           for (var j = 0, jotaro = _act.length; j < jotaro; j++ ) {
 
-            var page = {date: _act[j].date, begin: 0, end: 0, boss: 0, spend: 0, info: []}, count = 0
+            var page = {date: _act[j].date,
+                        begin: 0,
+                        end: 0,
+                        boss: 0,
+                        spend: 0,
+                        info: [],
+                        add: 0,
+                        reduce: 0,
+                        addMes: [],
+                        reduceMes: []}, count = 0
 
             if(_act[j].begin.indexOf(nick) > -1) {
               total = total + 30
@@ -62,6 +90,34 @@
               total = total + 20
               page.end = 20
               count++
+            }
+
+            if (_act[j].add) {
+
+              for (var n = 0, johny = _act[j].add.length; n < johny; n++) {
+
+                if(_act[j].add[n].name === nick) {
+                  total = total + _act[j].add[n].point
+                  page.add = page.add + _act[j].add[n].point
+                  page.addMes.push(_act[j].add[n])
+                }
+
+              }
+
+            }
+
+            if (_act[j].reduce) {
+
+              for (var o = 0, joly = _act[j].reduce.length; o < joly; o++) {
+
+                if(_act[j].reduce[o].name === nick) {
+                  spend = spend + _act[j].reduce[o].point
+                  page.reduce = page.reduce + _act[j].reduce[o].point
+                  page.reduceMes.push(_act[j].reduce[o])
+                }
+
+              }
+
             }
 
             for (var k = 0, joseku = _act[j].kill.length; k < joseku; k++) {
