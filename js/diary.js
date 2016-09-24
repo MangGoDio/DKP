31297426fe2e216a4f8240c5cfd2b6e7075f29d3
黄书乐 <492179450@qq.com>
2016-09-24 19:29:21 +0800

    9月24日集合

--- js/diary.js
+++ js/diary.js
@@ -342,232 +342,59 @@ var _act = [
       ]
     }
   ]
+},
+{
+  id: 3, // 第一次活动
+  date: '2016.9.24', // 活动日期
+  begin: ['阿何','奇迹','西法','Des','小黑','蝌蚪王','猫粮','速度灭','小峰','风哥'
+          ,'言水','血魔','饭团','伤心','小七','满爷','滑稽','马静','前卫','今晚'
+          ,'樱井','吊爷','小灵','火歌','麦兜','尼好','悟饭'], // 集合分 30
+  off: ['老严','6BB'], // 请假分 10
+  end: [],
+  kill: []
 }
 ]
 
 var _gays = [
-  {
-     id: 1,
-     nick: '阿何',
-     name: '零下一度',
-     pro: '战士'
-  },
-  {
-     id: 2,
-     nick: '吊爷',
-     name: '吊爷不懒惰',
-     pro: '战士'
-  },
-  {
-     id: 3,
-     nick: '樱井',
-     name: '樱井智子丶',
-     pro: '盗贼'
-  },
-  {
-     id: 4,
-     nick: '麦兜',
-     name: 'Réinhàrdt',
-     pro: '圣骑士'
-  },
-  {
-     id: 5,
-     nick: '西法',
-     name: '西法灬',
-     pro: '圣骑士'
-  },
-  {
-     id: 6,
-     nick: '奇迹',
-     name: '奇迹吾身与共',
-     pro: '德鲁伊'
-  },
-  {
-     id: 7,
-     nick: '蝌蚪王',
-     name: '蝌蚪王',
-     pro: '德鲁伊'
-  },
-  {
-     id: 8,
-     nick: '踏足者',
-     name: '踏足者',
-     pro: '德鲁伊'
-  },
-  {
-     id: 9,
-     nick: '本泽马',
-     name: '本泽马丶',
-     pro: '德鲁伊'
-  },
-  {
-     id: 10,
-     nick: '猫粮',
-     name: '不吃猫粮的喵',
-     pro: '牧师'
-  },
-  {
-     id: 11,
-     nick: '满爷',
-     name: '漓墨',
-     pro: '猎人'
-  },
-  {
-     id: 12,
-     nick: '马静',
-     name: 'Imvback',
-     pro: '猎人'
-  },
-  {
-     id: 13,
-     nick: '火歌',
-     name: '火歌精灵',
-     pro: '猎人'
-  },
-  {
-     id: 14,
-     nick: '伤心',
-     name: '伤心绝望',
-     pro: '猎人'
-  },
-  {
-     id: 15,
-     nick: '风哥',
-     name: '紫色星星',
-     pro: '猎人'
-  },
-  {
-     id: 16,
-     nick: '血魔',
-     name: '丶卡尔丶',
-     pro: '法师'
-  },
-  {
-     id: 17,
-     nick: '小七',
-     name: 'Krystale',
-     pro: '术士'
-  },
-  {
-     id: 18,
-     nick: '6BB',
-     name: '柳哔哔',
-     pro: '术士'
-  },
-  {
-     id: 19,
-     nick: '喵星人',
-     name: '喵星人的救赎',
-     pro: '死亡骑士'
-  },
-  {
-     id: 20,
-     nick: 'Des',
-     name: 'Desperater',
-     pro: '恶魔猎手'
-  },
-  {
-     id: 21,
-     nick: '杯酱',
-     name: '杯酱',
-     pro: '恶魔猎手'
-  },
-  {
-     id: 22,
-     nick: '言水',
-     name: '言水',
-     pro: '恶魔猎手'
-  },
-  {
-     id: 23,
-     nick: '滑稽',
-     name: '方天滑稽',
-     pro: '恶魔猎手'
-  },
-  {
-     id: 24,
-     nick: '命运',
-     name: '命运宠儿',
-     pro: '恶魔猎手'
-  },
-  {
-     id: 25,
-     nick: '速度灭',
-     name: '速度灭灬',
-     pro: '萨满'
-  },
-  {
-     id: 26,
-     nick: '小黑',
-     name: '工口少女黄泉',
-     pro: '恶魔猎手'
-  },
-  {
-     id: 27,
-     nick: '小虾',
-     name: '蓝晓虾',
-     pro: '圣骑士'
-  },
-  {
-     id: 28,
-     nick: '小峰',
-     name: '随机食品',
-     pro: '德鲁伊'
-  },
-  {
-     id: 29,
-     nick: '黑锋',
-     name: '黑锋强盗',
-     pro: '死亡骑士'
-  },
-  {
-     id: 30,
-     nick: '悟饭',
-     name: 'Babycora',
-     pro: '圣骑士'
-  },
-  {
-     id: 31,
-     nick: '老严',
-     name: '叫我老严好了',
-     pro: '猎人'
-  },
-  {
-     id: 32,
-     nick: '二板',
-     name: '二板暴徒丶',
-     pro: '法师'
-  },
-  {
-     id: 33,
-     nick: '小灵',
-     name: '亡灬小灵',
-     pro: '盗贼'
-  },
-  {
-     id: 34,
-     nick: '冬冬',
-     name: '圣女冬冬',
-     pro: '圣骑士'
-  },
-  {
-     id: 35,
-     nick: '大H',
-     name: 'Hydrá',
-     pro: '牧师'
-  },
-  {
-    id: 36,
-    nick: '蘑菇',
-    name: '小鸡炒蘑菇',
-    pro: '术士'
-  },
-  {
-     id: 37,
-     nick: '前卫',
-     name: '前卫萧卡加',
-     pro: '恶魔猎手'
-  }
+  {id: 1,nick: '阿何',name: '零下一度',pro: '战士'},
+  {id: 2,nick: '吊爷',name: '吊爷不懒惰',pro: '战士'},
+  {id: 38,nick: '饭团',name: '冲锋的饭团儿',pro: '战士'},
+  {id: 4,nick: '麦兜',name: 'Réinhàrdt',pro: '圣骑士'},
+  {id: 30,nick: '悟饭',name: 'Babycora',pro: '圣骑士'},
+  {id: 5,nick: '西法',name: '西法灬',pro: '圣骑士'},
+  {id: 27,nick: '小虾',name: '蓝晓虾',pro: '圣骑士'},
+  {id: 6,nick: '奇迹',name: '奇迹吾身与共',pro: '德鲁伊'},
+  {id: 7,nick: '蝌蚪王',name: '蝌蚪王',pro: '德鲁伊'},
+  {id: 8,nick: '踏足者',name: '踏足者',pro: '德鲁伊'},
+  {id: 9,nick: '本泽马',name: '本泽马丶',pro: '德鲁伊'},
+  {id: 28,nick: '小峰',name: '随机食品',pro: '德鲁伊'},
+  {id: 10,nick: '猫粮',name: '不吃猫粮的喵',pro: '牧师'},
+  {id: 35,nick: '大H',name: 'Hydrá',pro: '牧师'},
+  {id: 15,nick: '风哥',name: '紫色星星',pro: '猎人'},
+  {id: 11,nick: '满爷',name: '漓墨',pro: '猎人'},
+  {id: 12,nick: '马静',name: 'Imvback',pro: '猎人'},
+  {id: 13,nick: '火歌',name: '火歌精灵',pro: '猎人'},
+  {id: 31,nick: '老严',name: '叫我老严好了',pro: '猎人'},
+  {id: 14,nick: '伤心',name: '伤心绝望',pro: '猎人'},
+  {id: 39,nick: '尼好',name: '尼好旅行者',pro: '猎人'},
+  {id: 40,nick: '今晚',name: '今晚猎的痛快',pro: '猎人'},
+  {id: 16,nick: '血魔',name: '丶卡尔丶',pro: '法师'},
+  {id: 25,nick: '速度灭',name: '速度灭灬',pro: '萨满'},
+  {id: 17,nick: '小七',name: 'Krystale',pro: '术士'},
+  {id: 18,nick: '6BB',name: '柳哔哔',pro: '术士'},
+  {id: 36,nick: '蘑菇',name: '小鸡炒蘑菇',pro: '术士'},
+  {id: 33,nick: '小灵',name: '亡灬小灵',pro: '盗贼'},
+  {id: 3,nick: '樱井',name: '樱井智子丶',pro: '盗贼'},
+  {id: 19,nick: '喵星人',name: '喵星人的救赎',pro: '死亡骑士'},
+  {id: 29,nick: '黑锋',name: '黑锋强盗',pro: '死亡骑士'},
+  {id: 26,nick: '小黑',name: '工口少女黄泉',pro: '恶魔猎手'},
+  {id: 20, nick: 'Des',name: 'Desperater',pro: '恶魔猎手'},
+  {id: 21,nick: '杯酱',name: '杯酱',pro: '恶魔猎手'},
+  {id: 22,nick: '言水',name: '言水',pro: '恶魔猎手'},
+  {id: 23,nick: '滑稽',name: '方天滑稽',pro: '恶魔猎手'},
+  {id: 24,nick: '命运',name: '命运宠儿',pro: '恶魔猎手'},
+  {id: 26,nick: '小黑',name: '工口少女黄泉',pro: '恶魔猎手'},
+  {id: 37,nick: '前卫',name: '前卫萧卡加',pro: '恶魔猎手'}
 ]
 
 var chinese = ['战士', '法师', '牧师', '死亡骑士', '圣骑士', '猎人', '盗贼', '恶魔猎手', '萨满', '武僧', '术士', '德鲁伊'],
